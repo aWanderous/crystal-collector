c468026d67b83db6422d8a6792bc0cc90830f87b
Alex Wan <a_wanderous@hotmail.com>
2019-08-20 21:43:30 +1000

    button functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,22 @@ $(document).ready(function() {
   $("#getNumber").html(target);
 
   // button clicked gives a random value for each button pressed
-  $("")
+  var lego = Math.floor(Math.random() * 12) + 1;
+  var plastic = Math.floor(Math.random() * 12) + 1;
+  var stones = Math.floor(Math.random() * 12) + 1;
+  var treasure = Math.floor(Math.random() * 12) + 1;
+  var counter = 0;
+
+  
+  
+  $("#legoBox, #plasticBox, #stonesBox, #treasureBox").on("click", function () {
+    var buttonId = $(this).attr('id');
+    console.log(buttonId);
+    counter = counter + lego;
+  });
+  
+ 
+
   // when boxes are clicked add the value together
 
   // if the value added together == the same as target number user wins
