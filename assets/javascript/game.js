026dc97ce9865329a09d7b11f2969c31aa9e4ca9
Alex Wan <a_wanderous@hotmail.com>
2019-08-20 21:00:07 +1000

    added target number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,17 @@
+$(document).ready(function() {
 
-function targetNumber() {
-  var x = Math.floor((Math.random() * 120) + 19);
-  document.getElementById("#getNumber").innerHTML = x;
-}
+  // target randomly generated
+  var target = Math.floor(Math.random() * 120) + 19;
+  $("#getNumber").html(target);
+
+  // button clicked gives a random value for each button pressed
+  $("")
+  // when boxes are clicked add the value together
+
+  // if the value added together == the same as target number user wins
+
+  // if the value does not add up to same target number then the user loses
+  // and will need to fight the bandits
+
+});
+
